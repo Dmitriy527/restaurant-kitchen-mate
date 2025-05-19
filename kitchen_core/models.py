dea53c25c2ee5ae02e3114f2538f943b680be9bc
Dmytro <dimkanividimka4@gmail.com>
2025-05-19 16:34:39 +0300

    create model Cook and add this model in settings.py

--- kitchen_core/models.py
+++ kitchen_core/models.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
 
 class DishType(models.Model):
@@ -9,3 +11,15 @@ class DishType(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+        ordering = ["username"]
+
+    def __str__(self):
+        return f"{self.username} {self.first_name} {self.last_name}"
