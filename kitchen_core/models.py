ee76d2132c81e6d0edf922b932ce56c34a3741a1
Dmytro <dimkanividimka4@gmail.com>
2025-05-19 16:48:14 +0300

    create model Dish

--- kitchen_core/models.py
+++ kitchen_core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.urls import reverse
@@ -23,3 +24,21 @@ class Cook(AbstractUser):
 
     def __str__(self):
         return f"{self.username} {self.first_name} {self.last_name}"
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="dishes"
+    )
+
+    def __str__(self):
+        return f"{self.name} (price: {self.price}, dish type:  {self.dish_type})"
