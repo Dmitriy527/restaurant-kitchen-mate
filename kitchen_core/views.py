176de2560b0ded438ab6b4a5216d35b3b08efdc8
Dmytro <dimkanividimka4@gmail.com>
2025-05-20 20:24:15 +0300

    adding the index function to the kitchen_core view

--- kitchen_core/views.py
+++ kitchen_core/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from kitchen_core.models import Cook, DishType, Dish
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_cooks = Cook.objects.count()
+    num_dish_types = DishType.objects.count()
+    num_dishes = Dish.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_dish_types": num_dish_types,
+        "num_dishes": num_dishes,
+        "num_visits": num_visits + 1,
+    }
+    return render(request, "kitchen/index.html", context=context)
