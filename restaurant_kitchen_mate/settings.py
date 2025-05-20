0a73757b31a289b3452d96dadc7fec6810b62901
Dmytro <dimkanividimka4@gmail.com>
2025-05-20 20:09:44 +0300

    connecting the static folder and redirecting the logged in user in the settings.py file

--- restaurant_kitchen_mate/settings.py
+++ restaurant_kitchen_mate/settings.py
@@ -124,9 +124,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "kitchen_core.Cook"
+
+LOGIN_REDIRECT_URL = "/"
