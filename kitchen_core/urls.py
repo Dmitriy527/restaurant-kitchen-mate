3f99a55ef26b8a850b54c50464d0c2e7adab58b6
Dmytro <dimkanividimka4@gmail.com>
2025-05-27 17:40:56 +0300

    Add path dish_types_list to url

--- kitchen_core/urls.py
+++ kitchen_core/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from kitchen_core.views import index
+from kitchen_core.views import (
+    index,
+    DishTypesList,
+
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("dish-types/", DishTypesList.as_view(), name="dish_types_list"),
 ]
 
 app_name = "kitchen_core"
