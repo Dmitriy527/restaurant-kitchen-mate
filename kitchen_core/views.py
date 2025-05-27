ab2f572716f7df67de73de3579dadbc3136bc11f
Dmytro <dimkanividimka4@gmail.com>
2025-05-27 22:31:20 +0300

    overriding the get_context_date function in the DishTypesListView to add the search_field to the context

--- kitchen_core/views.py
+++ kitchen_core/views.py
@@ -1,11 +1,10 @@
-from lib2to3.fixes.fix_input import context
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import generic
 
+from kitchen_core.forms import DishTypeSearchForm
 from kitchen_core.models import Cook, DishType, Dish
 
 
@@ -32,3 +31,11 @@ class DishTypesListView(LoginRequiredMixin, generic.ListView):
     context_object_name = "dish_types_list"
     template_name = "kitchen/dish_types_list.html"
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DishTypesListView, self).get_context_data(**kwargs)
+        dish_type = self.request.GET.get("name_dish_type")
+        context["search_field"] = DishTypeSearchForm(
+            initial={"name_dish_type": dish_type}
+        )
+        return context
