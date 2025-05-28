130d990997ead428417cdc9ca2bf168f56f71c4c
Dmytro <dimkanividimka4@gmail.com>
2025-05-28 16:06:11 +0300

    editing the path name dish_type_list and adding a new path dish-type-create

--- kitchen_core/urls.py
+++ kitchen_core/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from kitchen_core.views import (
     index,
     DishTypesListView,
+    DishTypesCreateView,
 
 )
 
 urlpatterns = [
     path("", index, name="index"),
-    path("dish-types/", DishTypesListView.as_view(), name="dish_types_list"),
+    path("dish-types/", DishTypesListView.as_view(), name="dish-types-list"),
+    path("dish-types/create/", DishTypesCreateView.as_view(), name="dish-types-create"),
 ]
 
 app_name = "kitchen_core"
