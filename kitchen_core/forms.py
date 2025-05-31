71acd26b59f3d57333bc92bb6b2379345da90f97
Dmytro <dimkanividimka4@gmail.com>
2025-05-31 18:55:42 +0300

    Add new form 'CookCreationForm' with validator of field 'years_of_expirience'

--- kitchen_core/forms.py
+++ kitchen_core/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from kitchen_core.models import Dish, Cook
 
@@ -35,6 +37,25 @@ class CookSearchForm(forms.Form):
             attrs={"placeholder": "search by username"}
         )
     )
+
+
+def validate_years_of_experience(year_of_experience):
+    if int(year_of_experience) < 0:
+        raise ValidationError("years of experience must be greater or equal to zero")
+
+    return year_of_experience
+
+
+class CookCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Cook
+        fields = UserCreationForm.Meta.fields + (
+            "first_name",
+            "last_name",
+            "years_of_experience",
+        )
+    def clean_years_of_experience(self):
+        return validate_years_of_experience(self.cleaned_data["years_of_experience"])
 
 
 class DishForm(forms.ModelForm):
