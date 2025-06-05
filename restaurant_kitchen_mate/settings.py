48a29ea33bf8adbfac0aa64080e5013bb74ad501
Dmytro <dimkanividimka4@gmail.com>
2025-06-05 10:41:56 +0300

    Saving SECRET_KEY in .env file, updating requirements.exe, adding SECRET_KEY to settings, adding .env to .gitignor

--- restaurant_kitchen_mate/settings.py
+++ restaurant_kitchen_mate/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pwnx9=%jj!&=7ukz0chpk787q@gx-#p13t$)1cpam9(c(ji7&)'
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
