c62e419f6dca5c7f2048c795ede4dc79c871b4e0
Dmytro <dimkanividimka4@gmail.com>
2025-06-11 02:14:52 +0300

    statikfiles managing

--- restaurant_kitchen_mate/settings/prod.py
+++ restaurant_kitchen_mate/settings/prod.py
@@ -6,7 +6,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
